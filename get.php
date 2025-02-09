37480e9616c024d9274b7aa27a389af6aea566d8
punirah <punirahfarel@gmail.com>
2025-02-09 09:43:39 +0700

    Update get.php

--- get.php
+++ get.php
@@ -3,7 +3,7 @@
 $abu =	"\e[0;33m";
 $abu1 = "\e[0;37m";
 $abu2 = "\e[1;30m";
-for ($time123 = 0; $time123 < 52000; $time123++) {
+for ($time123 = 0; $time123 < 2; $time123++) {
 $useragent = "Mozilla/5.0 (Linux; Android 6.0.1; NEO-U9-H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36 OPR/63.3.3216.58675";
 $data = '{"bot":"helo word"}';
 $url = 'https://webhook.site/a2d75222-4ff0-4069-afc9-3dc15538aef2';
