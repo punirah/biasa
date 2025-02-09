189fb7e999ad33b212d144624510e5e40779e2c0
punirah <punirahfarel@gmail.com>
2025-02-09 12:36:58 +0700

    Update get.php

--- get.php
+++ get.php
@@ -5,7 +5,7 @@ $abu1 = "\e[0;37m";
 $abu2 = "\e[1;30m";
 for ($time123 = 0; $time123 < 5; $time123++) {
 $useragent = "Mozilla/5.0 (Linux; Android 6.0.1; NEO-U9-H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36 OPR/63.3.3216.58675";
-$data = '{"bot":"helo word"}';
+$data = json_encode(["bot" => "helo word" . $time123]);
 $url = 'https://webhook.site/ac88e0ee-7ddf-4ca7-afbf-76d090d0eeab';
 $header = [
     "user-agent: ".$useragent,
